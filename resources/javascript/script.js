c13772cc458f28ff6f24e588b5211627be16bb0b
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-09-24 20:52:07 +0300

    Add part with my projects

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,11 +1,26 @@
 nav_links = document.querySelectorAll('.parts-item');
 
 nav_links.forEach(link => {
-    link.addEventListener('click', e => setActive(e));
+    link.addEventListener('click', e => setActive(e.target));
 });
 
-function setActive(event) {
+function setActive(target) {
     const previous_active = document.querySelector('.active');
     previous_active.classList.remove('active');
-    event.target.classList.add('active');
+    target.classList.add('active');
+}
+
+function getId() {
+    let link = window.location.href;
+    let hashtag_index = link.indexOf('#');
+    let id = undefined;
+    if (hashtag_index !== -1) id = link.slice(hashtag_index+1);
+    return id;
+}
+
+
+let id = getId();
+if (id !== undefined) {
+    let active = document.getElementById(`${id}-link`);
+    if(active) setActive(active);
 }
